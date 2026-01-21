1cf7d649fb224dbdf68e3552d4cddeaab8c22f89
Furkan Albayrak <furkanalb42@gmail.com>
2026-01-21 12:50:54 +0300

    Auth.js kurulumu yapildi

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+        port: '',
+        pathname: '/**', // Tüm alt yollara izin ver
+      },
+    ],
+  },
 };
 
 export default nextConfig;

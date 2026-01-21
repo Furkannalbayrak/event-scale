e5ba9c6b0bcbf65183a040df712c7438a967bc19
Furkan Albayrak <furkanalb42@gmail.com>
2026-01-21 18:23:15 +0300

    main layout ve page olusturuldu

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
 
 export default function Home() {
-  return (
-    <div>
+  console.log("DATABASE URL TEST:", process.env.DATABASE_URL); // <-- BU SATIRI EKLE
 
+  return (
+    <div className="text-center font-bold text-2xl mt-10">
+      Etkinlik Platformu
+      <p className="text-sm font-normal mt-4">Lütfen yukarıdan giriş yapın.</p>
     </div>
   );
 }
